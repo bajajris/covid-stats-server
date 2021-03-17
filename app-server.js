d9cf4e402bca8a07de0a2ff97d35ed52825de844
bajajris <bajajris@sheridancollege.ca>
2021-03-17 17:02:03 -0400

    Added routes

--- app-server.js
+++ app-server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const port = 8888;
+const data = require('./readFiles')
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,7 +11,43 @@ app.use((req, res, next) => {
 })
 
 app.get('/schoolcovidsummary', (req, res) => {
+    data.getSchoolCovidSummary().then((sData) => {
+        res.send(sData);
+    }, err => {
+        res.send(err);
+    })
+})
 
+app.get('/ongoing_outbreaks', (req, res) => {
+    data.getOngoingOutbreaks().then((sData) => {
+        res.send(sData);
+    }, err => {
+        res.send(err);
+    })
+})
+
+app.get('/schoolpartnersactivecovid', (req, res) => {
+    data.getSchoolPartnersActiveCovid().then((sData) => {
+        res.send(sData);
+    }, err => {
+        res.send(err);
+    })
+})
+
+app.get('/schoolcovidtesting', (req, res) => {
+    data.getSchoolCovidTesting().then((sData) => {
+        res.send(sData);
+    }, err => {
+        res.send(err);
+    })
+})
+
+app.get('/cases_by_status_and_phu', (req, res) => {
+    data.getCasesByStatusAndPHU().then((sData) => {
+        res.send(sData);
+    }, err => {
+        res.send(err);
+    })
 })
 
 app.listen(port, () => console.log(`Server running at localhost:
